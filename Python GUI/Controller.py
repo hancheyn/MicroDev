b6bb4cbdeb21cc4f68aa36c50b0dc23fbca5672e
nathan <hancheyn@mail.gvsu.edu>
2022-03-06 13:36:50 -0500

    Added Function Prototypes to Python

--- Python GUI/Controller.py
+++ Python GUI/Controller.py
@@ -2,14 +2,30 @@
 #
 #
 
+# Adjusting Globals:
+# https://www.geeksforgeeks.org/python-globals-function/#:~:text=Syntax%3A%20globals%20%28%29%20Parameters%3A%20No%20parameters%20required.%20Code,%3D%20c%20%2B%20a%20globals%28%29%20%5B%27a%27%5D%20%3D%20d
+ADC_PINS = 0
+GPIO_PINS = 0
+SLEEP_MODES = 0
+
+
 #
 def controller_init(string):
     print(string)
+    # Grab Data from Pin Config File
+    # Update global variables with this data
+    # GPIO Pin Amount
+    # Analog Pin Amount
+    # Sleep Modes Amount
     return "Something"
 
+# ******************************** Subject Tests ************************************
 
+
+# ******************************** User Input Reads ************************************
 #
 def start_read():
+
     print("Something")
 
 
@@ -23,6 +39,7 @@ def arrow_down_read():
     print("clear")
 
 
+# ******************************** Subject Board I/O ************************************
 #
 def subject_read():
     print("read")
