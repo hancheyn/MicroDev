708ab4bd2dfc77dbf1a20b671b2d528db260042f
nathan <hancheyn@mail.gvsu.edu>
2022-04-04 00:45:32 -0400

    Example Update

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
 
 
-print("HEllo")
+import subprocess
+
+#https://medium.com/@chaoren/how-to-timeout-in-python-726002bf2291
+import time
+from multiprocessing import Process
+
+
+def Timeout():
+    print('Start of function')
+    while True:
+        time.sleep(1)
+
+process = Process(target=Timeout, name='Timeout')
+process.start()
+process.join(timeout=2)
+
+process.terminate()
+if process.exitcode is None:
+    print(f'{process} Timeout!')
+
+
+ls_call = subprocess.run(["ls"], stdout=subprocess.PIPE, text=True)
+useless_call = subprocess.run(["cat"], stdout=subprocess.PIPE, text=True, input="Hello there?")
+print(useless_call.stdout)  # Hello from the other side
+print(ls_call.stdout)
+
+
+
