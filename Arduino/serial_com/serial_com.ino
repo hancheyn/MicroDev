a849628b0eb2c45bc63a4df29e0d538387d09dc9
dvetter97 <vetterd@mail.gvsu.edu>
2022-05-13 10:15:03 -0400

    Test

--- Arduino/serial_com/serial_com.ino
+++ Arduino/serial_com/serial_com.ino
@@ -3,8 +3,7 @@
  * Description: Serial Communication to Raspi Test
  * Author:  Nathan Hanchey
             Dylan Vetter
-            test
- * Date: May 5th 2022
+ * Date: May 13th 2022
  ************************************************************/
 
 // Function Prototypes
