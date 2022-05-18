ed2917577fb0d8ce8b0323048dd9e6e23447d770
nathan <hancheyn@mail.gvsu.edu>
2022-05-18 08:43:24 -0400

    Update to serial_com.ino

--- Arduino/serial_com/serial_com.ino
+++ Arduino/serial_com/serial_com.ino
@@ -23,6 +23,18 @@ void configure_sleep_mode(unsigned int sleepmode);
 void wakeUp();
 
 
+/* Facade and Actual Function Pointer Prototypes */
+//FACADE&ACTUAL FUNCTION POINTERS
+void (*test) (uint8_t, uint8_t);
+void (*p_pinMode) (uint8_t, uint8_t);
+void (*p_digitalWrite) (uint8_t, uint8_t);
+void (*p_analogWrite) (uint8_t, uint8_t);
+int (*p_digitalRead) (unsigned char);
+int (*p_analogRead) (unsigned char);
+//sleep modes pointers?
+
+
+//SETUP
 void setup() {
   
   Serial.begin(115200); //sets the baud rate
@@ -30,21 +42,30 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   pinMode(2, INPUT);   // digital sensor is on digital pin 2
+
+  //FACADE POINTER EXAMPLEs (setup)
+  //test = &digitalWrite;
+  p_digitalWrite = &digitalWrite;
+  p_digitalRead = &digitalRead;
+  p_pinMode = &pinMode;
+  p_analogRead = &analogRead;
+  //+Sleep Modes
 }
 
 unsigned char RMSG[3];
 
+// MAIN LOOP
 void loop() {
-  // 
-  
-  if (Serial.available() > 0) { //Serial.available() returns the number of bytes read
+
+  //FACADE FUNCTION POINTER EXAMPLE (LED)
+  //(*test)(13, HIGH);
+  //FACADE FUNCTION POINTER EXAMPLE (INPUT)
+  //Serial.println((*p_digitalRead)(2));
+
+  if (Serial.available() > 0) {
     //delay(50);
-    //Serial.println(Serial.available());   
-        
-        //inByte = Serial.read(); //Serial.read() returns the first available byte in the serial buffer, then removes it.
-        //delay(50);
-        //Serial.write(inByte); //Serial.write(x) writes binary data to the serial port as a byte or series of bytes
-     
+    //Serial.println(Serial.available());
+
         //Write Test
         command_read(RMSG);
         delay(50); //delay is important
@@ -56,11 +77,18 @@ void loop() {
           //Send Back Results
           command_write(RMSG[0], RMSG[1], RMSG[2]);
         }
+
   }
+
 }
 
 
+/* Command Read
+ *  Input & Output: data[] (encodes unsigned char array)
+ *
+ */
 int command_read(unsigned char data[]) {
+
    //HAL_UART_Receive(&huart2, data, 3, 10000);
    data[0] = Serial.read();
    delay(5);
@@ -69,9 +97,15 @@ int command_read(unsigned char data[]) {
    data[2] = Serial.read();
    delay(5);
 
-   return 0;
+  return 0;
 }
 
+/* Serial Comm Commands Write
+ * Parameters:
+ * pin (unsigned int) -> data byte 0
+ * result (unsigned int) -> data byte 1
+ * test (unsigned int) -> data byte 2
+ */
 int command_write(unsigned int pin, unsigned int result, unsigned int test) {
 
   //Write
@@ -87,6 +121,13 @@ int command_write(unsigned int pin, unsigned int result, unsigned int test) {
   return 0;
 }
 
+/* CRC Encoding
+ * Input & Output: data[] (encodes unsigned char array)
+ * Parameters:
+ * pin (unsigned int) -> data byte 0
+ * result (unsigned int) -> data byte 1
+ * test (unsigned int) -> data byte 2
+ */
 int crc_encode(unsigned char data[], unsigned int pin, unsigned int result, unsigned int test) {
 
   // Find the data
@@ -105,7 +146,10 @@ int crc_encode(unsigned char data[], unsigned int pin, unsigned int result, unsi
   return 0;
 }
 
-
+/* CRC Decoding and Error Checking
+ * Input & Output: data[] (decodes unsigned char array)
+ * Return Value: 1 indicates correct crc encoding & 0 indicates incorrect encoding
+ */
 int crc_decode(unsigned char data[]) {
 
   // Find the data
