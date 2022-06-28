672c4c3fcf2527159d6f7ad663b7ae6221057912
nathan <hancheyn@mail.gvsu.edu>
2022-06-27 22:37:59 -0400

    Updated Setup File for Github

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cp Python/Integration/Bigfoot.py Python
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
 f=$(pwd)
-export PATH=$PATH:$f/bin/arduino-cli
+export PATH=$PATH:$f/bin/
 arduino-cli core install arduino:avr
 arduino-cli board list
 
